b2063be5a5f747b9d8c40d8d5c3e150fe6d23ca6
Stas <stas@stas-G31T-M7.(none)>
2012-10-30 16:40:32 +0600

    3.c don't work

--- 3.c
+++ 3.c
@@ -1,31 +1,51 @@
 #include<stdio.h>
 #include<time.h>
 
-int patients()
-
-
-int main() {
-
-  int res;
+int patients(int *yy, int *mm, int *dd, int *hh1,  int *m1, int *s1, int *hh2, int *m2, int *s2, long int *reception) {
+ 
+  
   struct tm time1;
   struct tm time2;
 
-  time1.tm_year = 2012 - 1900;
-  time1.tm_mon = 6;
-  time1.tm_mday = 5;
-  time1.tm_hour = 12;
-  time1.tm_min = 30;
-  time1.tm_sec = 0;
+  time1.tm_year = yy - 1900;
+  time1.tm_mon = mm;
+  time1.tm_mday = dd;
+  time1.tm_hour = hh1;
+  time1.tm_min = m1;
+  time1.tm_sec = s1;
 
-  time2.tm_year = 2012 - 1900;
-  time2.tm_mon = 6;
-  time2.tm_mday = 5;
-  time2.tm_hour = 16;
-  time2.tm_min = 30;
-  time2.tm_sec = 0;
+  time2.tm_year = yy - 1900;
+  time2.tm_mon = mm;
+  time2.tm_mday = dd;
+  time2.tm_hour = hh2;
+  time2.tm_min = m2;
+  time2.tm_sec =s2;
   
-  res = ((mktime(&time2) - mktime(&time1))/60)%increment;
-  printf("Time is %d", res);
+  res = ((mktime(&time2) - mktime(&time1))/60)%reception; 
+
+  return res;
+}
+
+
+int main() {
+  
+  int result;
+  int year, month, day, hour1, hour2, min1, min2, sec1, sec2;
+  long int min;
+  year = 2012;
+  month = 6;
+  day = 5;
+  hour1 = 12;
+  min1 = 30;
+  sec1 = 0;
+  hour2 = 16;
+  min2 = 30;
+  sec2 = 0;
+  
+  min = 30;
+
+    result = patients(year, month, day, hour1, min1, sec1, hour2, min2, sec2, min);
+  printf("Number of patients %d", result);
 
 
   return 0;
