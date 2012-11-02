6fe42ea437011b8b867bba27acf0ba1a9ba82027
Stas <stas@stas-G31T-M7.(none)>
2012-11-02 10:23:52 +0600

    begin 2

--- 3.c
+++ 3.c
@@ -10,13 +10,13 @@ int patients(int time1_h, int time1_m, int time2_h, int time2_m, int increment) 
     /*  res = ((time2_h * MINS_IN_HOUR + time2_m) - (time1_h * MINS_IN_HOUR + time1_m))%increment;
       printf("Time is %d", res);
     */
-  for(; time1_h <= time2_h && time1_m <= time2_m; time1_m + increment) {
-    printf("%d:%d", time1_h, time1_m);
+  for(; time1_h <= time2_h && time1_m <= time2_m; time1_m = time1_m + increment) {
+
     if(time1_m >= MINS_IN_HOUR) {
       time1_h++;
       time1_m = time1_m - MINS_IN_HOUR;
     }
-    printf("%d : %d" \n, time1_h, time1_m);
+    printf("%d:%d \n", time1_h, time1_m);
   } 
 
   return 0;
