c40368a8bdb73411e018f5c871688d705a60cc30
Stas <stas@stas-G31T-M7.(none)>
2012-11-02 09:42:52 +0600

    it's just begin

--- 3.c
+++ 3.c
@@ -1,32 +1,40 @@
 #include<stdio.h>
-#include<time.h>
 
-int patients()
+#define MINS_IN_HOUR 60
+
+int patients(int time1_h, int time1_m, int time2_h, int time2_m, int increment) {
+  
+
+ 
+  
+    /*  res = ((time2_h * MINS_IN_HOUR + time2_m) - (time1_h * MINS_IN_HOUR + time1_m))%increment;
+      printf("Time is %d", res);
+    */
+  for(; time1_h <= time2_h && time1_m <= time2_m; time1_m + increment) {
+    printf("%d:%d", time1_h, time1_m);
+    if(time1_m >= MINS_IN_HOUR) {
+      time1_h++;
+      time1_m = time1_m - MINS_IN_HOUR;
+    }
+    printf("%d : %d" \n, time1_h, time1_m);
+  } 
+
+  return 0;
+}
 
 
 int main() {
-
-  int res;
-  struct tm time1;
-  struct tm time2;
-
-  time1.tm_year = 2012 - 1900;
-  time1.tm_mon = 6;
-  time1.tm_mday = 5;
-  time1.tm_hour = 12;
-  time1.tm_min = 30;
-  time1.tm_sec = 0;
-
-  time2.tm_year = 2012 - 1900;
-  time2.tm_mon = 6;
-  time2.tm_mday = 5;
-  time2.tm_hour = 16;
-  time2.tm_min = 30;
-  time2.tm_sec = 0;
   
-  res = ((mktime(&time2) - mktime(&time1))/60)%increment;
-  printf("Time is %d", res);
+  
+  int hour_1 = 13;
+  int min_1 = 0;
+  int hour_2 = 15;
+  int min_2 = 0;
 
+  int min = 30; 
+ 
+  patients(hour_1, min_1, hour_2, min_2, min);
+ 
 
   return 0;
 }
