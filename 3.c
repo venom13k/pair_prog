ed4e096e9e8e1ac4d3816d6fd6fa0317006ab8be
Stas <stas@stas-G31T-M7.(none)>
2012-11-13 12:10:17 +0600

    not end

--- 3.c
+++ 3.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
 #define MINS_IN_HOUR 60
 
 struct simple_time {
     int hour;
     int min;
-    int to_min;
+  int to_min;
     char str;
 };
 
@@ -16,28 +18,32 @@ int time_to_min(int h, int m) {
   return h*MINS_IN_HOUR + m;
 }
 
-char *time_to_str(int i_m, int h, int m) {
-  char *s = '\0';
+char *time_to_str(int t_m, int h, int m) {
+  char s[7];
   
-  h = i_m % MINS_IN_HOUR;
-  m = i_m - h*MINS_IN_HOUR;
+  h = t_m / MINS_IN_HOUR;
+  m = t_m - h*MINS_IN_HOUR;
 
-  sprintf(s, "%d:%d", h, m);
+  sprintf(s, "%d:%d ", h, m);
   return s;
 }
 
 
-int patients(time *tm_1, time *tm_2, int increment) {
+char *time_for_patients(time *tm_1, time *tm_2, int increment) {
   
-  char *s;
+  int n = (time_to_min(tm_2->hour, tm_2->min) - time_to_min(tm_1->hour, tm_1->min)) / increment * 7;// number of patients
+  char *str;
+
+  str = malloc(n*sizeof(char));
+
 
   for(tm_1->to_min = time_to_min(tm_1->hour, tm_1->min); tm_1->to_min <= time_to_min(tm_2->hour, tm_2->min); tm_1->to_min += increment) {
-    
-    //    s = time_to_str(tm_1->to_min, tm_1->hour, tm_1->min);
-    printf("%d \n", tm_1->to_min);
+
+    str = strcat(str, time_to_str(tm_1->to_min, tm_1->hour, tm_1->min));
   }
-	
-  return 0;
+  
+  return str;
+  free(str);
 }
 
 
@@ -53,7 +59,7 @@ int main() {
 
   int min = 30; 
  
-  patients(&time1, &time2, min);
+  printf("%s\n", time_for_patients(&time1, &time2, min));
  
 
   return 0;
