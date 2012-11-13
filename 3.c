71f8345778e409d3d51ee9b575c30fc46ea31e78
Stas <stas@stas-G31T-M7.(none)>
2012-11-13 09:29:33 +0600

    using typedef

--- 3.c
+++ 3.c
@@ -2,12 +2,14 @@
 
 #define MINS_IN_HOUR 60
 
-  struct time {
+struct simple_time {
     int hour;
     int min;
-    int in_min;
+    int to_min;
     char str;
-  };
+};
+
+typedef struct simple_time time;  
 
 int time_to_min(int h, int m) {
   
@@ -25,12 +27,14 @@ char *time_to_str(int i_m, int h, int m) {
 }
 
 
-int patients(struct time *tm_1, struct time *tm_2, int increment) {
+int patients(time *tm_1, time *tm_2, int increment) {
   
-  for(tm_1->in_min = time_to_min(tm_1->hour, tm_1->min); tm_1->in_min <= time_to_min(tm_2->hour, tm_2->min); tm_1->in_min += increment) {
+  char *s;
+
+  for(tm_1->to_min = time_to_min(tm_1->hour, tm_1->min); tm_1->to_min <= time_to_min(tm_2->hour, tm_2->min); tm_1->to_min += increment) {
     
-    //    time_to_str(tm_1.in_min, tm_1.hour, tm_1.min);
-    printf("%d \n", tm_1->in_min);
+    //    s = time_to_str(tm_1->to_min, tm_1->hour, tm_1->min);
+    printf("%d \n", tm_1->to_min);
   }
 	
   return 0;
@@ -39,8 +43,8 @@ int patients(struct time *tm_1, struct time *tm_2, int increment) {
 
 int main() {
   
-  struct time time1;
-  struct time time2;
+  time time1;
+  time time2;
 
   time1.hour = 13;
   time1.min = 15;
