57c1618d2eba69829e3cfbf85247db683865326d
Stas_Porotikov <venom13k@mail.ru>
2012-11-17 18:49:47 +0600

    3.c: using struct time in functions and dynamic memory for time strings

--- 3.c
+++ 3.c
@@ -14,25 +14,24 @@ struct simple_time {
 
 typedef struct simple_time time;  
 
-int time_to_min(int h, int m) {
+int time_to_min(time *t) {
   
-  return h*MINS_IN_HOUR + m;
+  return (t->hour*MINS_IN_HOUR + t->min);
 }
 
-char *time_to_str(int t_m) {
-  static char s[5];
+char *min_to_time(time *t) {
 
-  int h;
-  int m;
-  
-  h = t_m / MINS_IN_HOUR;
-  m = t_m - h*MINS_IN_HOUR;
+  char *s;
+  s = malloc(7*sizeof(char));
 
-  if (h >= HOURS_IN_DAY) {
-    h = h - HOURS_IN_DAY;
+  t->hour = t->to_min / MINS_IN_HOUR;
+  t->min = t->to_min - t->hour*MINS_IN_HOUR;
+
+  if (t->hour >= HOURS_IN_DAY) {
+    t->hour = t->hour - HOURS_IN_DAY;
   }
 
-  sprintf(s, "%d:%02d ", h, m);
+  sprintf(s, "%d:%02d ", t->hour, t->min);
   return s;
 }
 
@@ -48,6 +47,7 @@ char *time_for_patients(time *tm_1, time *tm_2, int increment) {
   
   int n;
   char *str;
+  char *one_time;
 
   if (tm_1->hour >= HOURS_IN_DAY || tm_2->hour >= HOURS_IN_DAY) {
     std_err();
@@ -59,17 +59,18 @@ char *time_for_patients(time *tm_1, time *tm_2, int increment) {
   }
 
 
-  n = (time_to_min(tm_2->hour, tm_2->min) - time_to_min(tm_1->hour, tm_1->min)) / increment * 7;// number of patients
+  n = (time_to_min(tm_2) - time_to_min(tm_1)) / increment * 7;// number of patients
+
   str = malloc(n * sizeof(char));
   
-  for(tm_1->to_min = time_to_min(tm_1->hour, tm_1->min); tm_1->to_min <= time_to_min(tm_2->hour, tm_2->min); tm_1->to_min += increment) {
+  for(tm_1->to_min = time_to_min(tm_1); tm_1->to_min <= time_to_min(tm_2); tm_1->to_min += increment) {
     
-    str = strcat(str, time_to_str(tm_1->to_min));
-    
+    one_time = min_to_time(tm_1);
+    str = strcat(str, one_time);
+    free(one_time);
   }
-  
+
   return str;
-  free(str);
   }
 
 
@@ -91,16 +92,19 @@ int main() {
   
   time time1;
   time time2;
+  
+  char *s;
 
   time1.hour = 23;
   time1.min = 55;
   time2.hour = 13;
   time2.min = 50;
 
-  int min = 15; 
- 
-  printf("%s\n", time_for_patients(&time1, &time2, min));
- 
+  int min = 15;
+  
+  s = time_for_patients(&time1, &time2, min);
+  printf("%s\n", s);
+  free(s);
 
   return 0;
 }
