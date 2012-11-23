5c1ab6ff6044153287a1b6c988802d880cc0a85b
Stas <stas@stas-G31T-M7.(none)>
2012-11-23 12:54:17 +0600

    function 'min_to_time'return time

--- 3.c
+++ 3.c
@@ -45,12 +45,16 @@ int time_to_min(time *t) {
   return (t->hour*MINS_IN_HOUR + t->min);
 }
 
-void min_to_time(int m, time *t) {
+time min_to_time(int m) {
 
-  t->hour = m / MINS_IN_HOUR;
-  t->min = m % MINS_IN_HOUR;
+  time t;
 
-  printf("Time: %02d:%02d\n", t->hour, t->min);
+  t.hour = m / MINS_IN_HOUR;
+  t.min = m % MINS_IN_HOUR;
+
+  printf("Time: %02d:%02d\n", t.hour, t.min);
+  
+  return t;
 }
 
 time  *time_for_patients(time *tm_1, time *tm_2, int increment) {
@@ -65,11 +69,11 @@ time  *time_for_patients(time *tm_1, time *tm_2, int increment) {
 
   tm[0] = *tm_1;
   minutes = time_to_min(tm);
-
+  printf("tm_1:%d  tm_2:%d \n", time_to_min(tm_1), time_to_min(tm_2));
   for( i = 0; i <= n; i++) {
- 
-    printf("%d:%d %d\n", tm->hour, tm->min, minutes);
-    min_to_time(minutes, &(tm[i]));
+    
+    printf("%d:%d %d\n", tm[i].hour, tm[i].min, minutes);
+    tm[i] = min_to_time(minutes);
     minutes += increment;
   }
 
